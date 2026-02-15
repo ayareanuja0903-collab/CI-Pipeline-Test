77400a68f78b95803807dd0e1f3ca1ab50c325ff
Anuja <anujapawar791@gmail.com>
2026-02-15 17:50:06 +0530

    update UI Content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Hello, World!</h1>;
+const App = () => <h1>Hello, World Data!!</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
